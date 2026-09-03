d0ce787236ee8aa7486ea4c3eac99acd3080dd98
LeahJKH <leahjb06.kodehode@gmail.com>
2026-09-03 10:26:42 +0200

    so far so good

--- app.js
+++ app.js
@@ -31,9 +31,55 @@ const drumAudio = [
     }
 ]
 
+// audio play function
 function audioPlay(audioUrl) {
     let audio = new Audio(audioUrl);
     audio.play()
 }
+// audio play function
 
+
+// keydown event audio play
+document.addEventListener("keydown", (e) => {
+let curraudio = drumAudio.filter((aud) => aud.key === e.key)
+audioPlay(curraudio[0].sound)
+})
+// keydown event audio play
+
+
+// img audio play
+const drumSImg = document.querySelector("#drumStickImg")
+const drumKImg = document.querySelector("#drumKitImg")
+const bongoImg = document.querySelector("#bongoImg")
+
+bongoImg.addEventListener("click", (e) => {
 audioPlay(drumAudio[0].sound)
+})
+
+drumSImg.addEventListener("click", (e) => {
+    let rollaudio = drumAudio.filter(a => a.name === "roll")
+    audioPlay(rollaudio[0].sound)
+})
+// img audio play
+
+
+// card created on document
+const btnCard = document.querySelector("#btnsCard")
+
+drumAudio.forEach((e) => {
+    const containInfo = document.createElement("div")
+    containInfo.className = "containedTxt"
+
+    const nameP = document.createElement("p")
+    const nameTxt = document.createTextNode(e.name)
+    nameP.append(nameTxt)
+
+    const keyP = document.createElement("p")
+    const keyTxt = document.createTextNode(e.key)
+    keyP.append(keyTxt)
+
+    containInfo.appendChild(nameP)
+    containInfo.appendChild(keyP)
+    btnCard.appendChild(containInfo)
+})
+// card created on document
